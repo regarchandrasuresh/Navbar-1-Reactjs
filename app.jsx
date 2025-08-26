6095bee1945269635e11d623b9bb6e1c0a0be8fc
Suresh Regar <144477857+regarchandrasuresh@users.noreply.github.com>
2025-08-26 11:43:30 +0530

    App.jsx

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import Product from "./Product.jsx"
+
+export default function App() {
+  return (
+    <div className="Two">
+<Product Title="Vivoy12" Description="Moonlight Blue,6gbRAM,B " oldprice="3000"   newprice="1000" />
+<Product Title="realmec53" Description="Moonlight Blue,6gbRAM," oldprice="6000" newprice="9000" />
+<Product Title="naarzo10" Description="Moonlight Blue,6gbRAM," oldprice="4000" newprice="6000"  />
+
+
+
+    </div>
+  );
+}
